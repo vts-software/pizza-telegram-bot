ece34c5188a03bd5ec18159c183457a4e3436c80
vts-software <via.tsiamruk@gmail.com>
2026-03-12 21:26:14 +0100

    Add User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+
+    telegram_id = models.BigIntegerField(unique=True)
+
+    username = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
