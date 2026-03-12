6768d9fb20e8c3c5f02294560a46d42786d58f83
vts-software <via.tsiamruk@gmail.com>
2026-03-12 21:24:44 +0100

    Configure installed apps

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+'drf_yasg',
+
+'users',
+'menu',
+'cart',
+'orders',
+'bot',
+
 ]
 
 MIDDLEWARE = [
