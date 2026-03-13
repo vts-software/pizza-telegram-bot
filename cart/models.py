f283e219eacbf5d1283c593ccbf807c1a2d688d8
vts-software <via.tsiamruk@gmail.com>
2026-03-13 19:35:22 +0100

    Add cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from menu.models import PizzaSize
+
+
+class CartItem(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="cart_items",
+    )
+
+    pizza_size = models.ForeignKey(
+        PizzaSize,
+        on_delete=models.CASCADE,
+    )
+
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.user} - {self.pizza_size}"# Create your models here.
